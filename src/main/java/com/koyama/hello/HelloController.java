2a8201bca489f92774f818192ce22afeca5390c2
yoshi-koyama <koyama@reytech.co.jp>
2023-12-09 16:17:14 +0900

    /hello のレスポンスを JSON オブジェクトに変更

--- src/main/java/com/koyama/hello/HelloController.java
+++ src/main/java/com/koyama/hello/HelloController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-            return "hello koyama";
+    public HelloResponse hello() {
+            return new HelloResponse("hello world");
     }
 
 }
